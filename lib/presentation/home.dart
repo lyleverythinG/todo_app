92829d18cc2dbc7394b7df089446ae8a96a8448c
lyleverythinG <lylecarcedo@gmail.com>
2022-09-04 15:38:54 +0800

    separated the widget for cleaner code.

--- lib/presentation/home.dart
+++ lib/presentation/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todolist_hydrated_bloc/presentation/widgets/todo_list.dart';
 import '../bloc/bloc/todo_bloc.dart';
 import '../constants/constants.dart';
 
@@ -99,73 +100,6 @@ class _HomeState extends State<Home> {
           child: TodoList(),
         ),
       ),
-    );
-  }
-}
-
-class TodoList extends StatelessWidget {
-  const TodoList({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<TodoBloc, TodoState>(
-      builder: (context, state) {
-        if (state is TodoLoading) {
-          return Container(
-            color: Colors.white,
-            child: const Center(child: CircularProgressIndicator()),
-          );
-        }
-        if (state is TodoResult) {
-          if (state.todoList.isEmpty) {
-            return const Center(
-              child: Text(
-                "You currently have no todo's",
-                style: TextStyle(fontSize: 1, fontWeight: FontWeight.bold),
-              ),
-            );
-          }
-          return ListView.builder(
-            itemCount: state.todoList.length,
-            itemBuilder: (context, index) {
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Dismissible(
-                  background: Container(
-                    color: Colors.redAccent,
-                  ),
-                  key: Key(state.todoList[index].todoId),
-                  onDismissed: (direction) {
-                    context
-                        .read<TodoBloc>()
-                        .add(RemoveTodo(id: state.todoList[index].todoId));
-                  },
-                  child: CheckboxListTile(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30)),
-                      activeColor: Colors.black87,
-                      tileColor: Colors.blueAccent,
-                      title: Text(
-                        state.todoList[index].todoTitle,
-                        style: state.todoList[index].isCompleted
-                            ? const TextStyle(
-                                decoration: TextDecoration.lineThrough)
-                            : null,
-                      ),
-                      value: state.todoList[index].isCompleted,
-                      onChanged: (value) async {
-                        context.read<TodoBloc>().add(
-                              UpdateTodoState(
-                                  value!, state.todoList[index].todoId),
-                            );
-                      }),
-                ),
-              );
-            },
-          );
-        }
-        return Container();
-      },
     );
   }
 }
